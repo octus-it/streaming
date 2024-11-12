df2eaf385598b714037eb27ae82d2f5025368062
Aldrich Clément D. <clement.djidjoho@wisourcing.com>
2024-11-12 12:37:31 +0000

    Mise a jour de la page d'accueil

--- src/app/shared/footer/footer-level2/footer-level2.component.ts
+++ src/app/shared/footer/footer-level2/footer-level2.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-footer-level2',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './footer-level2.component.html',
   styleUrl: './footer-level2.component.scss'
 })
--- src/app/shared/headers/navbar-withoutbg/navbar-withoutbg.component.ts
+++ src/app/shared/headers/navbar-withoutbg/navbar-withoutbg.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-navbar-withoutbg',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './navbar-withoutbg.component.html',
   styleUrl: './navbar-withoutbg.component.scss'
 })
 export class NavbarWithoutbgComponent {
 
+  activeBackdrop = false
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    console.log('Scroll position:', scrollPosition);
+
+    this.activeBackdrop = scrollPosition > 150;
+
+    // Tu peux ici appeler une méthode ou mettre à jour des variables en fonction du scroll
+  }
+
+  activeTab: string = 'home';
+
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
+
+  onScroll() {
+    console.log("je scroll")
+  }
 }
